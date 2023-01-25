bcf1bf09d18f11a694f3242b180ac45af577d37f
legion088 <ilnar_08@mail.ru>
2023-01-25 15:22:06 +0400

    first commit

---
+++ check_hosts_available.py
@@ -0,0 +1,44 @@
+import asyncio
+import aioping
+
+
+async def check_available_hosts(hostname: str):
+    try:
+        await aioping.ping(hostname) * 1000
+        print("UP", hostname)
+    except TimeoutError:
+        print("DOWN:", hostname)
+    except Exception as err:
+        print(f"Возникло исключение для {hostname}", err)
+
+
+def check_validation_lines(lines: list) -> list:
+    if not lines:
+        raise ValueError("Файл пустой!")
+    forbidden_characters = [
+        ',', '~', ':', '!', ' ', '@', '#',
+        '$', '%', '^', '&', '`', '.', '()',
+        '{}', '_'
+    ]
+
+    if any(char in line for char in forbidden_characters for line in lines):
+        raise ValueError("Строка содержит запрещающий символ!")
+    return [line.strip() for line in lines]
+
+
+def open_readlines_file(file: str) -> list:
+    with open(file, "r") as f:
+        return f.readlines()
+
+
+def run(hosts: list):
+    coroutines = [check_available_hosts(host) for host in hosts]
+    tasks = [asyncio.ensure_future(coroutine) for coroutine in coroutines]
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(*tasks))
+
+
+if __name__ == '__main__':
+    lines = open_readlines_file("hosts.txt")
+    hosts = check_validation_lines(lines)
+    run(hosts)
---
+++ tests/test_check_hosts.py
@@ -0,0 +1,29 @@
+import pytest
+from my_packages.check_hosts_available import *
+
+
+def test_check_validations():
+    lines = ['server01', 'server-02', 'server03']
+    file_lines = open_readlines_file("allowed.txt")
+    assert check_validation_lines(file_lines) == lines
+
+
+def test_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        open_readlines_file(" ")
+
+
+def test_check_validations_forbidden_symbols():
+    with pytest.raises(ValueError) as f_emp:
+        check_validation_lines([
+            ',', '~', ':', '!', ' ', '@', '#',
+            '$', '%', '^', '&', '`', '.', '()',
+            '{}', '_'
+        ])
+    assert "Строка содержит запрещающий символ!" == f_emp.value.args[0]
+
+
+def test_check_validations_empty_file():
+    with pytest.raises(ValueError) as f_emp:
+        check_validation_lines([])
+    assert "Файл пустой!" == f_emp.value.args[0]
